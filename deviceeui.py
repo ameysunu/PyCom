d9f78c09dbef7fe8dd982c2510b8716d05c99de5
ameysunu <ameysunu00719@gmail.com>
2022-07-17 20:48:50 +0100

    region added to deviceeui

--- deviceeui.py
+++ deviceeui.py
@@ -1,5 +1,5 @@
 from network import LoRa
 import ubinascii
 
-lora = LoRa(mode=LoRa.LORAWAN)
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 print('Device EUI:' + ubinascii.hexlify(lora.mac()).upper().decode('utf-8'))
