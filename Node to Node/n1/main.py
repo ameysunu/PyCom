caf5827253d5950486dec95d00e480421f5e4c82
ameysunu <ameysunu00719@gmail.com>
2022-07-17 22:41:22 +0100

    temperature and humidity files added

--- Node to Node/n1/main.py
+++ Node to Node/n1/main.py
@@ -1,15 +1,29 @@
 from network import LoRa
 import socket
 import time
+import ubinascii
+import struct
+import machine
+import uos
+import pycom
+import sys
+
+from pysense import Pysense
+from SI7006A20 import SI7006A20 # Temperature sensor
+
+py = Pysense()
+si = SI7006A20(py)
 
 # Please pick the region that matches where you are using the device
 
+temp = str(si.temperature())
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 i = 0
 while True:
-    s.send('Hi, This is Node 1')
+    s.send('Hi this is Node 1. My temperature is ' + temp)
     print(s.recv(64))
     i= i+1
     time.sleep(5)
--- Node to Node/n2/main.py
+++ Node to Node/n2/main.py
@@ -1,16 +1,30 @@
 from network import LoRa
 import socket
 import time
+import ubinascii
+import struct
+import machine
+import uos
+import pycom
+import sys
+
+from pysense import Pysense
+from SI7006A20 import SI7006A20 # Temperature sensor
+
+py = Pysense()
+si = SI7006A20(py)
 
 # Please pick the region that matches where you are using the device
 
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+humidity = str(si.humidity())
+
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 i = 0
 while True:
     print(s.recv(64));
-    s.send('Hi back from Node 2');
+    s.send('Hi back from Node 2. My humidity is ' + humidity);
     # if s.recv(64) == b'Ping':
     #     s.send('Pong')
     #     print('Pong {}'.format(i))
